5d94592cbd46d1ed5ab6903d685459f2ffa47e7c
syllesly <30665825+syllesly@users.noreply.github.com>
2018-06-07 18:59:00 +0200

    Update js.js

--- clicker/js.js
+++ clicker/js.js
@@ -4,6 +4,7 @@ var cost=10;
 function startup() {
   document.getElementById("clicks").innerHTML = "Clicks = "+clicks;
   document.getElementById("buy").innerHTML = "Buy clicks per click  "+cost+" clicks";
+  document.getElementById("cpc").innerHTML = "Clicks per clicks= "+cpc;
 }
 function klik() {
   clicks+=cpc;
