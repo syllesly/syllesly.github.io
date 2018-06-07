382e8d70c13544f41de02b224711a259595923bf
sylvester.keinicke <sylvester.keinicke@gmail.com>
2018-06-07 21:16:36 +0200

    test

--- clicker/js.js
+++ clicker/js.js
@@ -2,20 +2,22 @@ var clicks=0;
 var cpc=1;
 var cost=10;
 function startup() {
-  document.getElementById("clicks").innerHTML = "Clicks = "+clicks;
-  document.getElementById("buy").innerHTML = "Buy clicks per click  "+cost+" clicks";
-  document.getElementById("cpc").innerHTML = "Clicks per clicks= "+cpc;
+  write( "Clicks = "+clicks,"clicks");
+  document.getElementById("buy").innerHTML = "Price = "+cost+" clicks";
+  document.getElementById("cpc").innerHTML = "Clicks per click = "+cpc;
 }
 function klik() {
   clicks+=cpc;
   startup();
 }
 function buycpc() {
-  
   if(clicks>=cost) {
     clicks-=cost;
     cost = Math.round(Math.pow(cost,1.2));
     cpc++;
   }
   startup();
+}
+function write(txt,id) {
+  document.getElementById(id).innerHTML=txt;
 }
