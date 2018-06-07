4049e226af7dbef2232a9b5df16db6be6f9032bc
syllesly <30665825+syllesly@users.noreply.github.com>
2018-06-07 18:50:06 +0200

    Update js.js

--- clicker/js.js
+++ clicker/js.js
@@ -1,6 +1,19 @@
 var clicks=0;
-
+var cpc=1;
+var cost=10;
+function startup() {
+  document.getElementById("clicks").innerHTML = "Clicks = "+clicks;
+  document.getElementById("buy").innerHTML = "Buy clicks per click  "+cost+"clicks";
+}
 function klik() {
-  clicks++;
-  document.getElementById("id").innerHTML = "CLICKS = "+clicks;
+  clicks+=cpc;
+  document.getElementById("clicks").innerHTML = "Clicks = "+clicks;
+}
+function buycpc() {
+  if(clicks>=cost) {
+    clicks-=cost;
+    cost = pow(cost,1.1);
+    cpc++;
+  }
+  document.getElementById("buy").innerHTML = "Buy clicks per click  "+cost+"clicks";
 }
